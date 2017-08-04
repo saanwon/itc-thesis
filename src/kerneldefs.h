ffc09879283c0263a6492e900f23bb6ab90371f0
MoonSoo Kim <saanwon@gmail.com>
2017-08-04 10:27:43 +0900

    2017. 08. 04. (금) 10:27:48 KST

--- src/kerneldefs.h
+++ src/kerneldefs.h
@@ -1,21 +1,27 @@
 #ifndef _KERNELDEF_H_
 #define _KERNELDEF_H_
 
+#define USE_XCL_DATAFLOW
+
 #define NUM_RNN_LAYERS  2
 
+//#define RNN_CELL_SIZE     1500
 #define RNN_CELL_SIZE     200
 //#define RNN_CELL_SIZE		10
 
+#ifndef USE_XCL_DATAFLOW
 #if RNN_CELL_SIZE <= 32
 #define RNN_OCL_PIPE_DEPTH	32
 #elif RNN_CELL_SIZE <= 256
 #define RNN_OCL_PIPE_DEPTH	256
+#elif RNN_CELL_SIZE <= 2048
+#define RNN_OCL_PIPE_DEPTH	2048
 #endif
+#endif /* ! USE_XCL_DATAFLOW */
 
-//#define WORK_GROUP_SIZE		RNN_CELL_SIZE
-#define WORK_GROUP_SIZE		1
-
-
+/*
+ * FLAGS
+ */
 #define LSTM_FLAG_INIT_STATE	(1 << 0)
 
 #endif /* ! _KERNELDEF_H_ */
